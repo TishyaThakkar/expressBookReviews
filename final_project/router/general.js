ae694cc606baf9f4b9e2b89ab5616f0686afea7e
Tishya Thakkar <tishyathakkar@gmail.com>
2024-05-28 23:14:58 -0400

    Updated code with task 13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios').default;
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -26,10 +27,10 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',async function (req, res) {
     const isbn = req.params.isbn;
-    res.send(books[isbn])
- });
+    res.send(books[isbn]);
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -73,4 +74,76 @@ public_users.get('/review/:isbn',function (req, res) {
     res.send(books[isbn].review);
 });
 
+// Task 10
+async function getBooks() {
+    try {
+        const response = await axios.get('/');
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function getBookList(){
+    return new Promise((resolve,reject)=>{
+        resolve(books);
+    })
+}
+
+// Get all books – Using async callback function
+function getAllBooks() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        resolve(books);
+    }, 2000);
+    return;
+    });
+}
+
+// Search by ISBN – Using Promises
+function getBookByISBN(isbn) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        const book = books[isbn];
+        if (!book) {
+            reject("Book not found");
+        }
+        resolve(book);
+        }, 2000);
+    });
+}
+
+  // Search by author – Using async callback function
+function getBookByAuthor(author) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        const booksByAuthor = [];
+        for (const key in books) {
+            if (books[key].author === author) {
+                booksByAuthor.push(books[key]);
+            }
+        }
+        if (booksByAuthor.length === 0) {
+            reject("Book not found");
+        }
+        resolve(booksByAuthor);
+        }, 2000);
+    });
+}
+
+// Search by title – Using async callback function
+function getBookByTitle(title) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        for (const key in books) {
+            if (books[key].title === title) {
+                resolve(books[key]);
+            }
+        }
+        reject("Book not found");
+        }, 2000);
+    });
+}
+
 module.exports.general = public_users;
+module.exports.getBooks = getBooks;
